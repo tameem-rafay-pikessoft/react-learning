1a7e7ea6e930145b792dd7544d712f01540d3703
tameem-rafay <tameem.rafay@pikessoft.com>
2023-10-14 18:30:11 +0500

    use usestate

---
+++ src/components/NewPost.jsx
@@ -0,0 +1,25 @@
+import classes from './NewPost.module.css';
+import { useState } from 'react';
+function NewPost() {
+  let [currentValue, updateBody ] = useState('initial value');
+
+  function changeBodyHandler(event){
+    updateBody(event.target.value);
+    console.log(event.target.value);
+  }
+  return (
+    <form className={classes.form}>
+      <p>
+        <label htmlFor="body">Text</label>
+        <textarea id="body" required rows={3} onChange = {changeBodyHandler} />
+      </p>
+      <p> {currentValue} </p>
+      <p>
+        <label htmlFor="name">Your name</label>
+        <input type="text" id="name" required />
+      </p>
+    </form>
+  );
+}
+
+export default NewPost;
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,11 +1,15 @@
 import style from './PostList.module.css';
-import Post from './Post'
+import Post from './Post';
+import NewPost from '../components/NewPost'
 function PostList() {
     return (
-        <ul className =  {style.posts}>
-            <Post author="tameem" body="some content here" />
-            <Post author="usman" body="some content for usman" />
-        </ul>
+        <>
+            <NewPost />
+            <ul className={style.posts}>
+                <Post author="tameem" body="some content here" />
+                <Post author="usman" body="some content for usman" />
+            </ul>
+        </>
     )
 
 }
